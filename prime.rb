8981211e61adab8003004681e46e3e9f3889abc9
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-10-09 23:22:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+require 'pry'
+
+
+def prime?(number)
+  if number < 0 || number == 0 || number == 1
+    false
+  else
+    (2..number - 1).to_a.all? { |e| number % e != 0  }
+
+  end
+end
