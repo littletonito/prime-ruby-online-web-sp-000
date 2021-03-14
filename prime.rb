589b55b67b666008bbf732178d39e2e88ba2450f
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-14 15:34:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -10,6 +10,14 @@ def prime?(number)
     false
   else
     (2..number - 1).to_a.all? { |e| number % e != 0  }
+  end
+end
 
+
+def prime?(number)
+  if     number < 0 || number == 1 || number == 0
+    false
+  else
+    number % number == 1 || number % 1 == number
   end
 end
