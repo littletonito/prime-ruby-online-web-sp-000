561661b4456f55eb770314d04e97633a12716073
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-14 15:28:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,9 @@ require 'pry'
 
 
 def prime?(number)
+  # < 0 covers the negatives, ==0 cuz 0 cant be, ==1 cuz 1 isnt prime.
+  # So the 'number' that needs to be divided ranges from 2 til number-1 cuz
+  # any number divided by 1 or itslef will alwaays be true, therefore we dont inlcude 1 or itself
   if number < 0 || number == 0 || number == 1
     false
   else
